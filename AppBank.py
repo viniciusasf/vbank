57c66079a2fe2b55a166596d4d4439f9d126bfaa
viniciusasf <viniciusasferreira@hotmail.com>
2020-06-18 17:28:38 -0300

    Automatizei numero cliente e numero de ContaCorrente

--- AppBank.py
+++ AppBank.py
@@ -28,74 +28,70 @@ def sair():
     sys.exit()
 
 def novoCliente():
-    print('------- 1 - CADASTRO CLIENTE / C.C ---------------')
-    print()
-    cod = input('CODIGO DO CLIENTE: ')
+    print('------- 1 - CADASTRO CLIENTE / C.C ---------------\n')
+    qtdecliente = len(cliente['cod'])+1
+    cod = qtdecliente
     nome = input('NOME: ')
     nome.upper()
     telefone = input('TELEFONE: ')
     cidade = input('CIDADE: ')
     cidade.upper()
-    cc = input('N. CONTA-CORRENTE: ')
+    cc = cod
     depinicial = input('DEPÓSITO INICIAL: ')
     depinicial = float(depinicial)
-    print('')
     cliente['cod'].append(cod)
     cliente['nome'].append(nome)
     cliente['cidade'].append(cidade)
     cliente['telefone'].append(telefone)
     Contas['cod'].append(cc)
     Contas['saldo'].append(depinicial)
-    print('Cliente Cadastrado com Sucesso!!!')
-    print()
-    print('Cliente: {}, \nSeu Codigo é: {}, \nConta-Corrente Numero: {} foi criado com Sucesso!!!!, \nDepósito inicial de R$: {}'.format(nome, cod, cc, depinicial))
+    print('Cliente Cadastrado com Sucesso!!!\n')
+    print('Nome do Cliente: {} \nSeu Codigo é: {} \nConta-Corrente Numero: {} Criado com Sucesso!!!! \nDepósito inicial de R$: {}'.format(nome, cod, cc, depinicial))
     monta_menu(menu_principal)
 
 def consultaCliente():
-    print()
-    print('------------------------- 2. CONSULTA CLIENTE ----------------------')
-    consultar = input("\nQual o codigo do cliente?: ")
+    print('--------------- 2. CONSULTA CLIENTE ---------------\n')
+    consultar = input("Qual o codigo do cliente?: ")
     consultarV = consultar in cliente['cod']
     if consultarV:
         pos = cliente['cod'].index(consultar)
-        print('Cliente Localizado Com sucesso!')
+        print('Cliente Localizado Com sucesso!\n')
         print('')
-        print("Codigo \t Nome \t\t Cidade \t Telefone \t Saldo Atual")
-        print("  {0}    \t {1} \t {2} \t {3} \t {4} ".format(cliente['cod'][pos], cliente['nome'][pos], cliente['cidade'][pos],
+        print("Codigo \t Nome \t Cidade \t Telefone \t Saldo Atual")
+        print("  {0}    \t {1} \t {2} \t {3} \t {4} \n".format(cliente['cod'][pos], cliente['nome'][pos], cliente['cidade'][pos],
                                                 cliente['telefone'][pos], Contas['saldo'][pos]))
-        print('')
         monta_menu(menu_principal)
-
     else:
-        print('Cliente Não Localizado, tente novamente')
+        print('Cliente Não Localizado, tente novamente\n')
         monta_menu(menu_cadastro)
 
 def listaCliente():
     print('------3. LISTA DE CLIENTES -----')
-    print()
-    print("Codigo \t Nome \t\t Telefone")
-
-    for i in range(len(cliente)-1):
-        print(f"   {cliente['cod'][i]}  \t {cliente['nome'][i]} \t {cliente['telefone'][i]}")
-    print('')
-    qtdecliente = len(cliente['cod'])
-    print('Existem {} Cliente Cadastrados'.format(qtdecliente))
-    monta_menu(menu_principal)
+    print("Codigo \t Nome \t Telefone")
+    if len(cliente['cod']) < 1:
+        print('\nNão existe Cliente Cadastrado')
+        monta_menu(menu_cadastro)
+    else:
+        for i in range(len(cliente)-1):
+            print(i)
+            print(f"{cliente['cod'][i]}\t {cliente['nome'][i]} \t {cliente['telefone'][i]}")
+        qtdecliente = len(cliente)
+        print('Existem {} Cliente Cadastrados'.format(qtdecliente))
+        monta_menu(menu_cadastro)
 
 def transDeposito() -> object:
     print('---------- 3.1 REALIZANDO DEPÓSITO -----------')
-    consultar = input("\nInforme o codigo da conta em que deseja realizar o deposito: ")
+    consultar = input("\nInforme o codigo da conta em que deseja realizar o deposito: \n")
     consultarV = consultar in Contas['cod']
     if consultarV:
         pos = Contas['cod'].index(consultar)
-        print()
         print('Olá {} tudo bem?, \nSaldo atual da sua Conta-Corrente é de: {}'.format(cliente['nome'][pos], Contas['saldo'][pos]))  #Buquei cliente no banco de dados e imprimi
         deposito = float(input("\nInforme o valor do deposito: "))
         deposito = float(deposito)
         valor = Contas['saldo'][pos]
         valor = valor + deposito
         Contas['saldo'][pos] = valor
-        print('Deposito realizado com Sucesso!!!')
+        print('Deposito realizado com Sucesso!!!\n')
         print('Olá {} tudo bem?, \nSaldo atual da Conta é de R$: {}'.format(cliente['nome'][pos],
                                                                                            Contas['saldo'][pos]))
         monta_menu(menu_principal)
